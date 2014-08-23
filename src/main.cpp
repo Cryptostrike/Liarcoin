5dc280468647497aa36faf6f7b67187ff8f07234
adamw192@live.com <adamw192@live.com>
2014-08-23 02:35:53 +0100

    Added Merkel hash

--- src/main.cpp
+++ src/main.cpp
@@ -2806,7 +2806,7 @@ bool InitBlockIndex() {
         printf("%s\n", hash.ToString().c_str());
         printf("%s\n", hashGenesisBlock.ToString().c_str());
         printf("%s\n", block.hashMerkleRoot.ToString().c_str());
-        assert(block.hashMerkleRoot == uint256("0x97ddfbbae6be97fd6cdf3e7ca13232a3afff2353e29badfab7f73011edd4ced9"));
+        assert(block.hashMerkleRoot == uint256("d06673b7c78f9701b339efc5c90d6e4e3e812bbced6431d6c41291caa36d4c8b"));
         block.print();
         assert(hash == hashGenesisBlock);
 
